b1c3b8813275f99f88ffde54941625532a13052d
Eduardo Quirino <edu.3532@gmail.com>
2022-02-18 13:27:53 -0300

    Atualização lista de itens

--- projetos/Lista de Nomes/Lista de Nomes/Form1.Designer.cs
+++ projetos/Lista de Nomes/Lista de Nomes/Form1.Designer.cs
@@ -42,17 +42,18 @@ namespace Lista_de_Nomes
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 28);
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 16);
+            this.label1.Size = new System.Drawing.Size(158, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Item para a Lista";
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(146, 28);
+            this.txtNome.Location = new System.Drawing.Point(189, 25);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(100, 23);
+            this.txtNome.Size = new System.Drawing.Size(341, 23);
             this.txtNome.TabIndex = 1;
             this.txtNome.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
@@ -63,66 +64,75 @@ namespace Lista_de_Nomes
             this.lstNome.ItemHeight = 16;
             this.lstNome.Location = new System.Drawing.Point(28, 73);
             this.lstNome.Name = "lstNome";
-            this.lstNome.Size = new System.Drawing.Size(213, 276);
+            this.lstNome.Size = new System.Drawing.Size(365, 372);
             this.lstNome.Sorted = true;
             this.lstNome.TabIndex = 2;
+            this.lstNome.UseWaitCursor = true;
             this.lstNome.SelectedIndexChanged += new System.EventHandler(this.lstNome_SelectedIndexChanged);
             // 
             // btnAdiciona
             // 
-            this.btnAdiciona.Location = new System.Drawing.Point(251, 74);
+            this.btnAdiciona.BackColor = System.Drawing.Color.Chocolate;
+            this.btnAdiciona.Location = new System.Drawing.Point(399, 73);
             this.btnAdiciona.Name = "btnAdiciona";
             this.btnAdiciona.Size = new System.Drawing.Size(131, 32);
             this.btnAdiciona.TabIndex = 3;
             this.btnAdiciona.Text = "&Adicionar Item";
-            this.btnAdiciona.UseVisualStyleBackColor = true;
+            this.btnAdiciona.UseVisualStyleBackColor = false;
             this.btnAdiciona.Click += new System.EventHandler(this.btnAdiciona_Click);
             // 
             // btnRemove
             // 
-            this.btnRemove.Location = new System.Drawing.Point(251, 133);
+            this.btnRemove.BackColor = System.Drawing.Color.Chocolate;
+            this.btnRemove.Location = new System.Drawing.Point(399, 132);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(131, 32);
             this.btnRemove.TabIndex = 4;
             this.btnRemove.Text = "&Remover Item";
-            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.UseVisualStyleBackColor = false;
             this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
             // 
             // btnRemoveTudo
             // 
-            this.btnRemoveTudo.Location = new System.Drawing.Point(251, 194);
+            this.btnRemoveTudo.BackColor = System.Drawing.Color.Chocolate;
+            this.btnRemoveTudo.Location = new System.Drawing.Point(399, 193);
             this.btnRemoveTudo.Name = "btnRemoveTudo";
             this.btnRemoveTudo.Size = new System.Drawing.Size(131, 32);
             this.btnRemoveTudo.TabIndex = 5;
             this.btnRemoveTudo.Text = "Remover &Tudo";
-            this.btnRemoveTudo.UseVisualStyleBackColor = true;
+            this.btnRemoveTudo.UseVisualStyleBackColor = false;
             this.btnRemoveTudo.Click += new System.EventHandler(this.btnRemoveTudo_Click);
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(251, 255);
+            this.btnCancelar.BackColor = System.Drawing.Color.Chocolate;
+            this.btnCancelar.Location = new System.Drawing.Point(399, 254);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(131, 32);
             this.btnCancelar.TabIndex = 6;
             this.btnCancelar.Text = "&Cancela";
-            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.UseVisualStyleBackColor = false;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // btnSair
             // 
-            this.btnSair.Location = new System.Drawing.Point(251, 316);
+            this.btnSair.BackColor = System.Drawing.Color.Chocolate;
+            this.btnSair.Location = new System.Drawing.Point(399, 315);
             this.btnSair.Name = "btnSair";
             this.btnSair.Size = new System.Drawing.Size(131, 32);
             this.btnSair.TabIndex = 7;
             this.btnSair.Text = "&Sair";
-            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.UseVisualStyleBackColor = false;
             this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
             // 
             // FrmListadeNomes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 411);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(574, 453);
             this.Controls.Add(this.btnSair);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnRemoveTudo);
@@ -133,7 +143,7 @@ namespace Lista_de_Nomes
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "FrmListadeNomes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
